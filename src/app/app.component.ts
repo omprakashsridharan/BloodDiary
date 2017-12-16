56114ff9a40ad8151d53af80d2e6f7d9f2d68ee3
omprakash <omprakashsridharan@outlook.com>
2017-12-16 20:57:29 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+
+interface donor {
+  name: string;
+  phone: number;
+  group: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  donorCollection: AngularFirestoreCollection<donor>;
+  donors: Observable<donor[]>;
+  name:string;
+  phone:number;
+  group:string;
+
+  constructor(private afs: AngularFirestore){}
+  ngOnInit(){
+    this.donorCollection = this.afs.collection('donors');
+    this.donors = this.donorCollection.valueChanges();
+  }
+
+  addDonor(){
+    this.afs.collection('donors').add({'name': this.name, 'group': this.group,'phone':this.phone});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatCardModule, MatListModule, MatFormFieldModule, MatSelectModule} from '@angular/material';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDnrJoZ2GKbLisPx7JiBspgnHLHcRL06TA",
+  authDomain: "angular-firebase-demo-84d89.firebaseapp.com",
+  databaseURL: "https://angular-firebase-demo-84d89.firebaseio.com",
+  projectId: "angular-firebase-demo-84d89",
+  storageBucket: "angular-firebase-demo-84d89.appspot.com",
+  messagingSenderId: "936237151459"
+};
+
 
 
 @NgModule({
@@ -10,7 +25,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule,MatCardModule,MatListModule,MatFormFieldModule,MatSelectModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
